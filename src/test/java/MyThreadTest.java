5d2a59fdc64654615d22e059e3d9a3d463117ec4
Lenovo <laithoasaad71@gmail.com>
2023-10-31 22:52:35 +0300

    second version with 4 thread

--- src/test/java/MyThreadTest.java
+++ src/test/java/MyThreadTest.java
@@ -25,4 +25,28 @@ public class MyThreadTest extends TestCase {
             System.out.println("Number is not prime");
 
     }
+
+    public void testMyThread()
+    {
+        long start = System.currentTimeMillis();
+        int start_number = 1;
+        int end_number = 10000000;
+        List<MyThread> threadList = new ArrayList<MyThread>();
+        for(int i=0; i<4; i++)
+        {
+            MyThread thread = new MyThread("thread",(int)(((double)i/4)*end_number), (int)(((double)(i+1)/4)*end_number));
+            thread.start();
+            threadList.add(thread);
+        }
+        for(MyThread thread : threadList)
+        {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Total time is "+ (end-start)/1000+ " seconds");
+    }
 }
